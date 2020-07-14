7f83b0a23d379288447179b43da439adc051c869
Ben Kurtz <github+2020@benkurtz.org>
2020-07-14 12:46:30 -0700

    Run `prettier`

--- src/MapPanel.tsx
+++ src/MapPanel.tsx
@@ -4,19 +4,22 @@ import { MapOptions, ValuePolyProps, PolyMap } from 'types';
 
 interface Props extends PanelProps<MapOptions> {}
 
-function sql2dArrayStringToArray (a:string) {
+function sql2dArrayStringToArray(a: string) {
 	// a will be in the format {{x1,y1}, {x2,y2}, {x3,y3}}
-	return a.substring(2,a.length-2).split(`},{`).map(x=>x.split(`,`).map(y=>+y));
+	return a
+		.substring(2, a.length - 2)
+		.split(`},{`)
+		.map(x => x.split(`,`).map(y => +y));
 }
 
 function array_min(p: number[][]) {
-	return p.reduce( (a,b) => {
+	return p.reduce((a, b) => {
 		return [Math.min(a[0], b[0]), Math.min(a[1], b[1])];
 	});
 }
 
 function array_max(p: number[][]) {
-	return p.reduce( (a,b) => {
+	return p.reduce((a, b) => {
 		return [Math.max(a[0], b[0]), Math.max(a[1], b[1])];
 	});
 }
@@ -24,11 +27,13 @@ function array_max(p: number[][]) {
 class MapPoly extends PureComponent<ValuePolyProps> {
 	render() {
 		const { p, center, scale, value, maxValue } = this.props;
-		const polystr = p.map( (pt) => {
-			return String((pt[0]-center[0])*scale) + "," + String((pt[1]-center[1])*scale);
-		}).join(" ");
+		const polystr = p
+			.map(pt => {
+				return String((pt[0] - center[0]) * scale) + ',' + String((pt[1] - center[1]) * scale);
+			})
+			.join(' ');
 
-		return <polygon points={polystr} fill-opacity={value/maxValue}/>;
+		return <polygon points={polystr} fill-opacity={value / maxValue} />;
 	}
 }
 
@@ -36,14 +41,14 @@ export class MapPanel extends PureComponent<Props> {
 	render() {
 		const { options, data, width, height } = this.props;
 
-		const powerData = data.series.filter((x) => (x.refId==="A"));
-		const polyData = data.series.filter((x) => (x.refId==="B"));
+		const powerData = data.series.filter(x => x.refId === 'A');
+		const polyData = data.series.filter(x => x.refId === 'B');
 		// Get the configured map polygon coordinates, either from a data source (should be query B) or from the configuration
 		let polys: PolyMap = {};
-		if(polyData.length) {
+		if (polyData.length) {
 			let polyRaw = polyData[0].fields[1].values;
 			let polyId = polyData[0].fields[0].values;
-			for(let i = 0; i < polyId.length; i++) {
+			for (let i = 0; i < polyId.length; i++) {
 				polys[String(polyId.get(i))] = sql2dArrayStringToArray(polyRaw.get(i));
 			}
 		} else {
@@ -53,24 +58,24 @@ export class MapPanel extends PureComponent<Props> {
 		let poly_list = Object.keys(polys).map(k => polys[k]);
 		const min_coord = array_min(poly_list.map(array_min));
 		const max_coord = array_max(poly_list.map(array_max));
-		const center = [(min_coord[0]+max_coord[0])/2, (min_coord[1]+max_coord[1])/2];
-		const scale_x = width/(max_coord[0]-min_coord[0]);
-		const scale_y = height/(max_coord[1]-min_coord[1]);
+		const center = [(min_coord[0] + max_coord[0]) / 2, (min_coord[1] + max_coord[1]) / 2];
+		const scale_x = width / (max_coord[0] - min_coord[0]);
+		const scale_y = height / (max_coord[1] - min_coord[1]);
 		const scale = Math.min(scale_x, scale_y);
 
 		// compute maximum power
 		let max_power = 1.0; // TODO: eventually pull this from an option
 		// TODO: is doing .fields[0] okay, or do we need to look up which field to use based on name/time/etc
-		for(const p of powerData) {
-			max_power = p.fields[0].values.toArray().reduce((a,b) => Math.max(a,b), max_power)
+		for (const p of powerData) {
+			max_power = p.fields[0].values.toArray().reduce((a, b) => Math.max(a, b), max_power);
 		}
 
 		// generate polygons
-		const mapPolys = powerData.map( (p) => {
+		const mapPolys = powerData.map(p => {
 			let panelID: string = p.name as string;
 			let panelPoly: number[][] = polys[panelID];
 			const allPower = p.fields[0].values;
-			const power = allPower.get(allPower.length-1); // most recent power for now
+			const power = allPower.get(allPower.length - 1); // most recent power for now
 			return <MapPoly p={panelPoly} center={center} scale={scale} value={power} maxValue={max_power} />;
 		});
 
