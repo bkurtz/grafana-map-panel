f57ca68f33fe9ef3443342b94aa11b73cfed5952
Ben Kurtz <github+2020@benkurtz.org>
2020-07-14 15:50:46 -0700

    Upgrade to grafana7
    Mostly updating dependencies, but also:
    - no longer using tslint, so get rid of that config file
    - API for creating config editors has changed substantially; switch to the new API

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,12 @@
 import { PanelPlugin } from '@grafana/data';
 import { MapOptions, defaults } from './types';
 import { MapPanel } from './MapPanel';
-import { MapEditor } from './MapEditor';
 
-export const plugin = new PanelPlugin<MapOptions>(MapPanel).setDefaults(defaults).setEditor(MapEditor);
+export const plugin = new PanelPlugin<MapOptions>(MapPanel).setDefaults(defaults).setPanelOptions(builder => {
+	return builder.addTextInput({
+		path: 'text',
+		name: 'Simple text option',
+		description: 'Additional text to show on map',
+		defaultValue: '',
+	});
+});
